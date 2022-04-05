ef5b9bf4f764b5a209fddfccaba15cde3480975a
Than Htut Zaw <zgyi025@gmail.com>
2022-04-05 22:11:24 +0630

    some date and time

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+import { MdDarkMode } from "react-icons/md";
+import { MdLightMode } from "react-icons/md";
 function App() {
+  const [darkmode, setdarkmode] = useState(false);
+  const darkmodetoggle = () => {
+    setdarkmode( (darkmode)=> !darkmode)
+  }
+  let today = new Date().toLocaleTimeString();
+
+  let date = new Date().toLocaleDateString();
+
+  //  time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+
+  //or
+
+  //localTimeString function
+
+  const [clock, setclock] = useState();
+  const updateClock = () => {
+    //set clock here
+    today = new Date().toLocaleTimeString();
+    setclock(today);
+  };
+  setInterval(updateClock, 1000);
+
+  const [newdate, setnewdate] = useState();
+  const updateDate = () => {
+    date = new Date().toLocaleDateString();
+    setnewdate(date);
+  };
+  setInterval(updateDate, 1000);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="main container">
+        <header className="row">
+          <h1 className="logo">Cola</h1>
+
+          {darkmode ? (
+            <MdLightMode onClick={darkmodetoggle} className="button light-icon" />
+          ) : (
+            <MdDarkMode onClick={darkmodetoggle} className="button dark-icon"></MdDarkMode>
+          )}
+        </header>
+
+        <section className="today-section">
+          <div className="today-card">
+            <div className="row">
+              <h2>Today</h2>
+              <p>{date}</p>
+            </div>
+            <p className="today-clock">{clock}</p>
+          </div>
+        </section>
+      </section>
     </div>
   );
 }
