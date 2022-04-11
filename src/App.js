e17d234f5f8b4e880292bfbb137caccd79356adb
Than Htut Zaw <zgyi025@gmail.com>
2022-04-11 12:02:15 +0630

    finally darkmode function finished with local storage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Header from "./components/header";
 import Today from "./components/today";
 import Content from "./components/Content";
 function App() {
-  const [darkmode, setdarkmode] = useState(false);
+  const [darkmode, setdarkmode] = useState(
+    // darkmode
+    localStorage.getItem("dark") === "true"
+    //  ()=> localStorage.getItem('dark')
+  );
 
-  
-  
+  useEffect(() => {
+    localStorage.setItem("dark", darkmode);
+  }, [darkmode]);
+  useEffect(() => {
+    if (darkmode == true) {
+      console.log("false");
+      document.documentElement.removeAttribute("dark", "true");
+    } else {
+      console.log("true");
+      document.documentElement.setAttribute("dark", "true");
+    }
+  }, [darkmode]);
+  // useEffect(() => {
+  //   const data = localStorage.getItem('dark')
+  //   if(data){
+  //     setdarkmode(data)
+  //   }
+  // }, [darkmode]);
+
+  // useEffect( () => {
+
+  //   const saveLocal = () => {
+  //     localStorage.setItem('dark',darkmode)
+  //   }
+  //   saveLocal();
+  // }, [darkmode]);
+  //   const saveLocal =  () => {
+  //      localStorage.setItem("dark", darkmode)
+  //    }
+  //  useEffect(() => {
+  //    saveLocal()
+  //  });
 
   //  time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
 
@@ -18,15 +52,20 @@ function App() {
 
   const [clock, setclock] = useState("h:m:s");
   const [newdate, setnewdate] = useState();
-  
-  const [showmodal, setshowmodal] = useState(false)
+
+  const [showmodal, setshowmodal] = useState(false);
 
   return (
     <div className="App">
       <section className="main container">
-        <Header darkmode={darkmode} setdarkmode={setdarkmode}/>
+        <Header darkmode={darkmode} setdarkmode={setdarkmode} />
 
-        <Today clock={clock} setclock={setclock} newdate={newdate} setnewdate={setnewdate} />
+        <Today
+          clock={clock}
+          setclock={setclock}
+          newdate={newdate}
+          setnewdate={setnewdate}
+        />
 
         <Content showmodal={showmodal} setshowmodal={setshowmodal} />
       </section>
--- src/components/header.js
+++ src/components/header.js
@@ -1,40 +1,45 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import { MdDarkMode } from "react-icons/md";
 import { MdLightMode } from "react-icons/md";
 function Header({ darkmode, setdarkmode }) {
-  useEffect(() => {
-    
-      // if(darkmode){
-      //   console.log(true)
-
-      // }
-    
-  }, );
-
+  let theme;
+  if (localStorage) {
+    theme = localStorage.getItem("dark");
+  }
   const darkmodetoggle = () => {
-    setdarkmode((darkmode) => !darkmode);
-    if(darkmode){
-        console.log('changed to light mode')
-        document.documentElement.removeAttribute('dark','true')
-      }
-      else{
-        
-        console.log('changed to dark mode')
-        document.documentElement.setAttribute('dark','true')
-        
-        
+    //  setdarkmode((darkmode)=> !darkmode)
+    // (theme == true) ? setdarkmode(theme) : setdarkmode(theme)
+    // console.log(theme)
+    // setdarkmode((darkmode) => !darkmode);
+    // console.log(theme)
+    // setdarkmode( localStorage.getItem('dark'))
+    if (darkmode) {
+      setdarkmode((darkmode) => !darkmode);
+      // console.log('changed to light mode')
+      //  document.documentElement.removeAttribute('dark','true')
+      localStorage.setItem("dark", false);
+    } else {
+      setdarkmode((darkmode) => !darkmode);
+      // console.log('changed to dark mode')
+      // document.documentElement.setAttribute('dark','true')
+      localStorage.setItem("dark", true);
     }
   };
 
   return (
     <header className="row">
       <h1 className="logo">Cola</h1>
-
-      {darkmode ? (
+      {theme == "false" ? (
         <MdLightMode onClick={darkmodetoggle} className="button light-icon" />
       ) : (
         <MdDarkMode onClick={darkmodetoggle} className="button dark-icon" />
       )}
+
+      {/* {darkmode ? (
+        <MdLightMode onClick={darkmodetoggle} className="button light-icon" />
+      ) : (
+        <MdDarkMode onClick={darkmodetoggle} className="button dark-icon" />
+      )} */}
     </header>
   );
 }
