c71a5d4fb4aa08a83ecf9e7392f96c98361c45b8
Than Htut Zaw <zgyi025@gmail.com>
2022-04-21 14:52:16 +0630

    localstorage error fixed

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -26,121 +26,147 @@ function Lists(props) {
   const [currentEditCard, setcurrentEditCard] = useState(null);
   const [currentDeleteCard, setcurrentDeleteCard] = useState(null);
 
+  // if(localStorage.getItem('data')?.length){
+  //   console.log(data)
+  // }
   useEffect(() => {
+    // localStorage.setItem("data", JSON.stringify(data));
     updateLocal();
   }, []);
-  const updateLocal = () => {
-    // if (localStorage.getItem('data') === null) {
-    //   localStorage.setItem('data', JSON.stringify([]) );
-    // } else {
-    // const newdata = JSON.parse(localStorage.getItem('data'));
-    // if(newdata) setdataStored(newdata)
 
-    const newdata = JSON.parse(localStorage.getItem("data"));
-    setdata(newdata);
-    // }
+  useEffect(() => {
+    setLocal();
+  }, [data]);
+  // if(localStorage.getItem('data').length){
+
+  //   updateLocal()
+  // }
+  // else{
+  // }
+
+  // useEffect(() => {
+  //   JSON.parse(localStorage.getItem('data'))
+  // }, []);
+  // const updateLocal = () => {
+
+  //     const newdata = JSON.parse(localStorage.getItem("data")) || [];
+  //   setdata(newdata);
+  // };
+
+  const setLocal = () => {
+    localStorage.setItem("data", JSON.stringify(data));
+  };
+
+  const updateLocal = () => {
+    if (localStorage.getItem("data") === null) {
+      localStorage.setItem("data", JSON.stringify([]));
+    } else {
+      let newdata = JSON.parse(localStorage.getItem("data"));
+      setdata(newdata);
+    }
   };
   // useEffect(() => {
   //   localStorage.setItem("data", JSON.stringify(data))
   // }, [data]);
   // if (data?.length) localStorage.setItem("data", JSON.stringify(data));
-  useEffect(() => {
-    if (data?.length) localStorage.setItem("data", JSON.stringify(data));
-    
-  }, [data]);
+
+  // let newdata = JSON.parse(localStorage.getItem("data"));
+
+  // useEffect(() => {
+  //   setdata(newdata);
+  // }, []);
+
   // const stored = JSON.parse(localStorage.getItem('data'))
 
-  
+  if (data) {
+    return (
+      <>
+        {showmodal && (
+          <Modal
+            data={data}
+            setdata={setdata}
+            showmodal={showmodal}
+            setshowmodal={setshowmodal}
+            currentCard={currentCard}
+          />
+        )}
 
-  return (
-    <>
-      {showmodal && (
-        <Modal
-          data={data}
-          setdata={setdata}
-          showmodal={showmodal}
-          setshowmodal={setshowmodal}
-          currentCard={currentCard}
-        />
-      )}
+        {showeditmodal && (
+          <EditModal
+            data={data}
+            setdata={setdata}
+            currentEditCard={currentEditCard}
+            showeditmodal={showeditmodal}
+            setshoweditmodal={setshoweditmodal}
+          />
+        )}
+        {showdeletemodal && (
+          <DeleteModal
+            data={data}
+            setdata={setdata}
+            currentDeleteCard={currentDeleteCard}
+            showdeletemodal={showdeletemodal}
+            setshowdeletemodal={setshowdeletemodal}
+          />
+        )}
 
-      {showeditmodal && (
-        <EditModal
-          data={data}
-          setdata={setdata}
-          currentEditCard={currentEditCard}
-          showeditmodal={showeditmodal}
-          setshoweditmodal={setshoweditmodal}
-        />
-      )}
-      {showdeletemodal && (
-        <DeleteModal
-          data={data}
-          setdata={setdata}
-          currentDeleteCard={currentDeleteCard}
+        {data.map((item) => (
+          <div className={`content-card`} key={item.id}>
+            {/* <div className={`content-card ${opentools ? 'content-card-nav-active' : ''}`} key={item.id}> */}
+            {item.namedate !== "" ||
+            // if !== null this condition will not work
+            item.date !== "" ? (
+              <div className="pointer-none">
+                <Tools
+                  onClick={() => {}}
+                  showdeletemodal={showdeletemodal}
+                  setshowdeletemodal={setshowdeletemodal}
+                  setshoweditmodal={setshoweditmodal}
+                  setcurrentEditCard={setcurrentEditCard}
+                  setcurrentDeleteCard={setcurrentDeleteCard}
+                  id={item.id}
+                />
 
-          showdeletemodal={showdeletemodal}
-          setshowdeletemodal={setshowdeletemodal}
-        />
-      )}
-
-      
-      {data && data.map((item) => (
-        <div className={`content-card`} key={item.id}>
-          {/* <div className={`content-card ${opentools ? 'content-card-nav-active' : ''}`} key={item.id}> */}
-          {item.namedate !== "" ||
-          // if !== null this condition will not work
-          item.date !== "" ? (
-            <div className="pointer-none">
-              <Tools
-                onClick={()=>{
-
-                }}
-                showdeletemodal={showdeletemodal}
-                setshowdeletemodal={setshowdeletemodal}
-                setshoweditmodal={setshoweditmodal}
-                setcurrentEditCard={setcurrentEditCard}
-                setcurrentDeleteCard={setcurrentDeleteCard}
-                id={item.id}
-              />
-
-              <li
-                className="cursor-default"
-                onClick={(e) => {
-                  setcurrentCard(item.id);
-                }}
-              >
-                <div className="content-children">
-                  <div className="content-row">
-                    {/* <p className="date-name">{localStorage.getItem('data').namedate}</p>
+                <li
+                  className="cursor-default"
+                  onClick={(e) => {
+                    setcurrentCard(item.id);
+                  }}
+                >
+                  <div className="content-children">
+                    <div className="content-row">
+                      {/* <p className="date-name">{localStorage.getItem('data').namedate}</p>
                     <p>{localStorage.getItem('data').date}</p> */}
-                    <p className="date-name">{item.namedate}</p>
-                    <p>{item.date}</p>
+                      <p className="date-name">{item.namedate}</p>
+                      <p>{item.date}</p>
+                    </div>
                   </div>
-                </div>
-              </li>
-            </div>
-          ) : (
-            <div>
-              <li
-                className="hover-list"
-                onClick={(e) => {
-                  setshowmodal((showmodal) => !showmodal);
-                  setcurrentCard(item.id);
-                }}
-              >
-                <div className="content-children">
-                  <div>
-                    <GrAdd className="add-btn" />
+                </li>
+              </div>
+            ) : (
+              <div>
+                <li
+                  className="hover-list"
+                  onClick={(e) => {
+                    //  localStorage.setItem("data", JSON.stringify(data));
+
+                    setshowmodal((showmodal) => !showmodal);
+                    setcurrentCard(item.id);
+                  }}
+                >
+                  <div className="content-children">
+                    <div>
+                      <GrAdd className="add-btn" />
+                    </div>
                   </div>
-                </div>
-              </li>
-            </div>
-          )}
-        </div>
-      ))}
-    </>
-  );
+                </li>
+              </div>
+            )}
+          </div>
+        ))}
+      </>
+    );
+  }
 }
 
 export default Lists;
--- src/components/header.js
+++ src/components/header.js
@@ -1,58 +1,75 @@
-import React, {useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { MdDarkMode } from "react-icons/md";
 import { MdLightMode } from "react-icons/md";
 function Header() {
-
+  // const [darkmode, setdarkmode] = useState(
+  //   localStorage.getItem("dark") === "true"
+  // );
+  let theme;
   const [darkmode, setdarkmode] = useState(
     localStorage.getItem("dark") === "true"
+    // localStorage.getItem('dark')
   );
-  let theme;
+
   if (localStorage) {
     theme = localStorage.getItem("dark");
   }
-  useEffect(() => {
-    if (theme === "true") {
-      // alert(`${theme} from local is dark-true`)
-      document.documentElement.removeAttribute("dark", "true");
-    } else {
-      // alert(`${theme} dark-false from local is `)
-      document.documentElement.setAttribute("dark", "true");
-    }
-  }, [theme]);
+ 
+    // if (theme === "true") {
+    //   document.documentElement.setAttribute("dark", "false");
+    // } else {
+    //   document.documentElement.setAttribute("dark", "true");
+    // }
   
+  // useEffect(() => {
+  //   if (theme === "true") {
+  //     document.documentElement.setAttribute("dark", "false");
+  //   } else {
+  //     document.documentElement.setAttribute("dark", "true");
+  //   }
+  // }, []);
+  // useEffect(() => {
+  //   console.log(theme)
+  //   if (theme === "false") {
+  //     document.documentElement.setAttribute("dark", "false");
+  //   } else {
+  //     document.documentElement.setAttribute("dark", "true");
+  //   }
+  // }, [theme]);
+
+  useEffect(() => {}, [darkmode]);
+
   useEffect(() => {
-    if (darkmode === true) {
+    // localStorage.setItem("dark", darkmode);
+
+    if (darkmode === false) {
       document.documentElement.removeAttribute("dark", "true");
+      // localStorage.setItem('dark','false')
     } else {
       document.documentElement.setAttribute("dark", "true");
+      // localStorage.setItem('dark','true')
     }
   }, [darkmode]);
 
-  useEffect(() => {
-    localStorage.setItem("dark", darkmode);
-  }, [darkmode]);
-
-  
-  if (localStorage) {
-    theme = localStorage.getItem("dark");
-  }
   const darkmodetoggle = () => {
     if (darkmode) {
-      setdarkmode((darkmode) => !darkmode);
-      document.documentElement.removeAttribute("dark", "true");
-      localStorage.setItem("dark", false);
+      // alert('false')
+      setdarkmode((prevstate) => !prevstate);
+      // document.documentElement.removeAttribute("dark", "true");
+      localStorage.setItem("dark", "false");
     } else {
-      setdarkmode((darkmode) => !darkmode);
-      document.documentElement.setAttribute("dark", "true");
-      localStorage.setItem("dark", true);
+      // alert('true')
+      setdarkmode((prevstate) => !prevstate);
+      // document.documentElement.setAttribute("dark", "true");
+      localStorage.setItem("dark", "true");
     }
   };
 
   return (
     <header className="row">
       <h1 className="logo">Cola</h1>
-      {theme === "false" ? (
-      <MdLightMode onClick={darkmodetoggle} className="button light-icon" />
+      {darkmode ? (
+        <MdLightMode onClick={darkmodetoggle} className="button light-icon" />
       ) : (
         <MdDarkMode onClick={darkmodetoggle} className="button dark-icon" />
       )}
