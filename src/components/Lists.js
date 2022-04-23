7df751b139d08a2ec738d8c1b2c1ba1dc931e889
Than Htut Zaw <zgyi025@gmail.com>
2022-04-23 14:26:32 +0630

    card-date ui updated

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -53,6 +53,28 @@ function Lists(props) {
   //   setdata(newdata);
   // };
 
+  
+  let today = new Date().toLocaleDateString();
+  const dateSubtract = () => {
+    // let today = new Date();
+    // let myPastDate = new Date(today)
+    // let getToday = myPastDate();
+
+    // return today;
+    
+    // const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+    // const utc1 = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
+    // const utc2 = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
+
+    // return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+  }
+  // let year = today.getFullYear()
+  // let month = today.getMonth()
+  // let day = today.getDay()
+  // let getToday = year+'-'+month+'-'+day
+    // setnewdate(date);
+
   const setLocal = () => {
     localStorage.setItem("data", JSON.stringify(data));
   };
@@ -138,7 +160,25 @@ function Lists(props) {
                       {/* <p className="date-name">{localStorage.getItem('data').namedate}</p>
                     <p>{localStorage.getItem('data').date}</p> */}
                       <p className="date-name">{item.namedate}</p>
-                      <p>{item.date}</p>
+                      {/* <p>{item.date}</p> */}
+                      <div className="content row">
+                        <div className="year">
+                          <p>2</p>
+                          <p>years</p>
+                        </div>
+                        <div className="month">
+                          <p>2</p>
+                          <p>months</p>
+                        </div>
+                        <div className="day">
+                          <p>2</p>
+                          <p>days</p>
+                        </div>
+                      </div>
+                      
+{}
+                      
+                      <p></p>
                     </div>
                   </div>
                 </li>
@@ -155,7 +195,7 @@ function Lists(props) {
                   }}
                 >
                   <div className="content-children">
-                    <div>
+                    <div className="add-btn-row">
                       <GrAdd className="add-btn" />
                     </div>
                   </div>
--- src/components/today.js
+++ src/components/today.js
@@ -1,4 +1,5 @@
 import React,{useEffect, useState} from "react";
+import Lists from "./Lists";
 
 function Today() {
 
